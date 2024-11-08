2ad7790c820b1d51c8f91c0a4f241691bb8b8619
123 <123@qq.com>
2024-11-08 12:47:23 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Suspense } from 'react'
 import Image from 'next/image'
 import { ellipsisFormat } from './utils'
 import copy from 'copy-to-clipboard'
@@ -20,7 +21,7 @@ const uls = [
   },
 ]
 
-export default function Home() {
+const ReferrerInfo = () => {
   const address: string = useSearchParams().get('referrer_address') ?? ''
 
   const _copy = () => {
@@ -31,9 +32,25 @@ export default function Home() {
         title: 'Copy Success !',
         showConfirmButton: false,
       })
-    } catch (error) {}
+    } catch (error) {
+      console.error(error)
+    }
   }
+  return (
+    <div
+      className="w-full text-[#FFFEFC] font-[Albert Sans] text-base"
+      style={{}}
+    >
+      <h2 className="font-semibold">Referrer’s Address:</h2>
+      <div className="flex w-full max-w-full overflow-hidden items-center justify-between">
+        <span className="w-52 middle-truncate">{ellipsisFormat(address)}</span>
+        <div className="w-28 h-10 bg-copy bg-no-repeat" onClick={_copy} />
+      </div>
+    </div>
+  )
+}
 
+export default function Home() {
   const _download = () => {
     Swal.fire({
       title: 'Coming Soon...',
@@ -42,90 +59,81 @@ export default function Home() {
   }
 
   return (
-    <div className="px-[18px] items-center justify-items-center min-h-screen py-9 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-hb-pattern-mobile bg-no-repeat bg-cover">
-      <main className="flex flex-col gap-8 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/creamstream.svg"
-          alt="Cream Stream Logo"
-          width={254}
-          height={138}
-          priority
-        />
-
-        <ul className="grid grid-cols-3 gap-2">
-          {uls.map((x) => (
-            <li className="p-[14px] bg-[#52353C] rounded-lg" key={x.text}>
-              <i
-                className={`flex w-full h-16 ${x.icon} bg-no-repeat bg-center`}
-              />
-              <span className="flex mt-2 text-[#FBEDE0] text-center font-[Albert Sans] text-base font-medium">
-                {x.text}
-              </span>
-            </li>
-          ))}
-        </ul>
-
-        <div
-          className="w-full text-[#FFFEFC] font-[Albert Sans] text-base"
-          style={{}}
-        >
-          <h2 className="font-semibold">Referrer’s Address:</h2>
-          <div className="flex w-full max-w-full overflow-hidden items-center justify-between">
-            <span className="w-52 middle-truncate">
-              {ellipsisFormat(address)}
-            </span>
-            <div className="w-28 h-10 bg-copy bg-no-repeat" onClick={_copy} />
-          </div>
-        </div>
-
-        <div className="flex items-start p-2 rounded-lg bg-[#191012]">
+    <Suspense fallback={null}>
+      <div className="px-[18px] items-center justify-items-center min-h-screen py-9 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-hb-pattern-mobile bg-no-repeat bg-cover">
+        <main className="flex flex-col gap-8 items-center sm:items-start">
           <Image
             className="dark:invert"
-            src="/tip.svg"
-            alt="Tip"
-            width={20}
-            height={20}
+            src="/creamstream.svg"
+            alt="Cream Stream Logo"
+            width={254}
+            height={138}
             priority
           />
-          <span className="text-[#FFFEFC] font-extrabold font-[Albert Sans] ml-2">
-            Copy the Referrer’s Address carefully. You will need this when you
-            sign up for your account.
-          </span>
-        </div>
 
-        <div className="flex flex-col items-center">
-          <button
-            className="flex items-center btn-shadow w-fit my-4 bg-UI-Color-Indigo-200 px-[32px] py-[16px] rounded-full text-UI-Color-Neutral-100 text-[18px] font-extrabold"
-            onClick={_download}
-          >
-            <Image
-              className="mr-[12px]"
-              src="/ios.svg"
-              priority
-              alt=""
-              width={18}
-              height={18}
-            />
-            Download for IOS
-          </button>
+          <ul className="grid grid-cols-3 gap-2">
+            {uls.map((x) => (
+              <li className="p-[14px] bg-[#52353C] rounded-lg" key={x.text}>
+                <i
+                  className={`flex w-full h-16 ${x.icon} bg-no-repeat bg-center`}
+                />
+                <span className="flex mt-2 text-[#FBEDE0] text-center font-[Albert Sans] text-base font-medium">
+                  {x.text}
+                </span>
+              </li>
+            ))}
+          </ul>
 
-          <button
-            className="flex items-center btn-shadow w-fit my-4 bg-UI-Color-Indigo-200 px-[32px] py-[16px] rounded-full text-UI-Color-Neutral-100 text-[18px] font-extrabold"
-            onClick={_download}
-          >
+          <ReferrerInfo />
+
+          <div className="flex items-start p-2 rounded-lg bg-[#191012]">
             <Image
-              className="mr-[12px]"
-              src="/android.svg"
+              className="dark:invert"
+              src="/tip.svg"
+              alt="Tip"
+              width={20}
+              height={20}
               priority
-              alt=""
-              width={18}
-              height={18}
             />
-            Download for Android
-          </button>
-        </div>
-      </main>
-    </div>
+            <span className="text-[#FFFEFC] font-extrabold font-[Albert Sans] ml-2">
+              Copy the Referrer’s Address carefully. You will need this when you
+              sign up for your account.
+            </span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <button
+              className="flex items-center btn-shadow w-fit my-4 bg-UI-Color-Indigo-200 px-[32px] py-[16px] rounded-full text-UI-Color-Neutral-100 text-[18px] font-extrabold"
+              onClick={_download}
+            >
+              <Image
+                className="mr-[12px]"
+                src="/ios.svg"
+                priority
+                alt=""
+                width={18}
+                height={18}
+              />
+              Download for IOS
+            </button>
+
+            <button
+              className="flex items-center btn-shadow w-fit my-4 bg-UI-Color-Indigo-200 px-[32px] py-[16px] rounded-full text-UI-Color-Neutral-100 text-[18px] font-extrabold"
+              onClick={_download}
+            >
+              <Image
+                className="mr-[12px]"
+                src="/android.svg"
+                priority
+                alt=""
+                width={18}
+                height={18}
+              />
+              Download for Android
+            </button>
+          </div>
+        </main>
+      </div>
+    </Suspense>
   )
 }
