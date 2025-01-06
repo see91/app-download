bc733aa25b6b6dffbe7d10c5d4526d400ffea8bc
123 <123@qq.com>
2025-01-06 10:14:23 +0800

    feat: :sparkles: implement middleware to rewrite IPFS requests to HTTP

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "8.219.11.39", // HTTP 资源的域名
-        port: "8080",
-        pathname: "/**", // 匹配所有路径
-      },
-    ],
-  },
   async rewrites() {
     return [
       {
