f402365c0eefc978e06fc4421311f14020646005
123 <123@qq.com>
2025-01-06 10:05:04 +0800

    feat: :sparkles: add remote image configuration for external resources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "8.219.11.39", // HTTP 资源的域名
+        port: "8080",
+        pathname: "/**", // 匹配所有路径
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
